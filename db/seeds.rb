0962367944422522aefdabeb36a8d4d40b38cf96
Eriko Kohatsu <erikokohatsu@gmail.com>
2021-05-26 13:05:57 +0200

    Added code for challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+
+# puts "Creating restaurants..."
+# dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+# pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+# [ dishoom, pizza_east ].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+# puts "Finished!"
+
+require 'faker'
+
+puts 'Creating 5 fake restaurants...'
+categories = %w(chinese italian japanese french belgian)
+5.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: categories.sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'

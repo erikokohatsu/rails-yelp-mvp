b54334d93833fe8acade91edca6748a64d8cfdac
Eriko Kohatsu <erikokohatsu@gmail.com>
2021-05-26 17:00:04 +0200

    added code for challenge

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,32 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    # 1. Create a review instance with the review_strong_params
+    @review = Review.new(review_strong_params)
+    # 2. Get the restaurant instance thanks to the id in the URL
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # 3. Attach the restaurant instance from the url to the review that we're creating
+    @review.restaurant_id = @restaurant.id #hardest step of the lecture
+    #review.restaurant = resto
+    # 4. save the review
+    if @review.save
+    #save returns a boolean: returns true or false
+    #5. redirect the user to the show page of the restaurant. In this show page, we're going to display all the reviews attached to this specific restaurant
+      redirect_to restaurant_path(@restaurant) #http request
+    else
+      render :new #rendering an action
+    end
+  end
+
+  private
+
+  def review_strong_params
+    params.require(:review).permit(:content, :rating)
+  end
+
 end
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
+  RATING =[0, 1, 2, 3, 4, 5]
   belongs_to :restaurant
   validates :content, presence:true
   validates :rating, presence:true, numericality: {greater_than_or_equal_to: 0}
